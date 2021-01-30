0174b699feadce73b19d8574470e5170eedc49d8
EngiNe_Man_055 <51885679+Shafiuzzaman-khan@users.noreply.github.com>
2021-01-30 22:35:16 +0600

    Add files via upload

---
+++ Pattern_4.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pattern_4 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of rows");
+		int N = sc.nextInt();
+		
+		for(int i=1;i<=N;i++) {
+			
+			for(int j=N-1;j>=i;j--) {
+				System.out.print(" ");
+			}
+			
+			for(int k=1;k<=i;k++) {
+				System.out.print("*");
+			}
+			
+			for(int l=2;l<=i;l++) {
+				
+				System.out.print("*");
+			}
+			
+				System.out.println();
+			}
+			
+			
+			
+		}
+
+	}
+
+
+
